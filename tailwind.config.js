b7a60c8470cf09af3dc32f4274b2829dee5bdd80
Christopher <odeniyichristopher7@gmail.com>
2023-12-05 00:28:33 +0000

    commiting the completed mobile view of the TechStudio API page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {},
+  },
+
+  fontFamily: {
+    'montserrat': ['Montserrat']
+  },
+  fontFamily: {
+    'inter': ['Inter']
+  },
+  plugins: [],
+}
+
